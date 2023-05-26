c263a45581c5fefb93890518259bb8d84ce31f4c
zhangfuzeng <zhangfuzeng@cangoonline.com>
2023-05-26 09:55:23 +0800

    用户自定义数据库源

---
+++ maple-input-db/src/main/java/com/maple/inputdb/MapleInputDbApplication.java
@@ -0,0 +1,20 @@
+package com.maple.inputdb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author 笑小枫 <https://xiaoxiaofeng.com/>
+ */
+@ServletComponentScan
+@MapperScan("com.maple.inputdb.mapper")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MapleInputDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleInputDbApplication.class, args);
+    }
+}
