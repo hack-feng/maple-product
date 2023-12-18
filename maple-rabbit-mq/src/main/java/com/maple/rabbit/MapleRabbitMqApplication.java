6cebe9462fd660dfafa19fbec94de1acdc3d0897
zhangfuzeng <zhangfuzeng@cangoonline.com>
2023-12-18 15:14:17 +0800

    🍁添加整合RabbitMQ的实例

---
+++ maple-rabbit-mq/src/main/java/com/maple/rabbit/MapleRabbitMqApplication.java
@@ -0,0 +1,16 @@
+package com.maple.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 笑小枫 <https://xiaoxiaofeng.com/>
+ */
+@SpringBootApplication
+public class MapleRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleRabbitMqApplication.class, args);
+    }
+
+}
