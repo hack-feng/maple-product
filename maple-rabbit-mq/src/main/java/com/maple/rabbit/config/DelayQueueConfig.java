ec4fb0d5431731ab0df6ca0d45e2ed3152cfba65
zhangfuzeng <zhangfuzeng@cangoonline.com>
2023-12-20 16:24:43 +0800

    🍁添加整合RabbitMQ的实例

--- maple-rabbit-mq/src/main/java/com/maple/rabbit/config/DelayQueueConfig.java
+++ maple-rabbit-mq/src/main/java/com/maple/rabbit/config/DelayQueueConfig.java
@@ -12,18 +12,20 @@ import org.springframework.context.annotation.Configuration;
 public class DelayQueueConfig {
     @Bean
     public Queue delayDeadQueue() {
-        return QueueBuilder.durable("delayDeadQueue")
-                .withArgument("x-dead-letter-exchange", "delay-exchange")
+        return QueueBuilder
+                .durable("delayDeadQueue")
+                .deadLetterExchange("delay-exchange")
                 .build();
     }
 
     @Bean
     public Queue delayQueue() {
         // 声明该队列的死信消息发送到的 交换机 (队列添加了这个参数之后会自动与该交换机绑定,并设置路由键,不需要开发者手动设置)
-        // x-message-ttl = 5000，设置TTL，单位ms，5s 内没被消费，则进入死信队列
-        return QueueBuilder.durable("delayQueue")
-                .withArgument("x-message-ttl", 5000)
-                .withArgument("x-dead-letter-exchange", "delay-exchange")
+        // ttl = 5000，设置TTL，单位ms，5s 内没被消费，则进入死信队列
+        return QueueBuilder
+                .durable("delayQueue")
+                .ttl(5000)
+                .deadLetterExchange("delay-exchange")
                 .build();
     }
 
