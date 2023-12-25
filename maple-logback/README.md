51de24b675dc54d6155e8eb73dc4bc2cd3298029
笑小枫 <1150640979@qq.com>
2023-12-25 17:25:40 +0800

    🍁 添加自定义配置 Logback 实例

---
+++ maple-logback/README.md
@@ -0,0 +1,7 @@
+## 模块简介
+
+Logback 旨在作为流行的 log4j 项目的继承者，是SpringBoot内置的日志处理框架，spring-boot-starter其中包含了spring-boot-starter-logging，该依赖内容就是 Spring Boot 默认的日志框架 logback。
+
+本模块主要演示Logback日志打印保存如何自定义的一个实例。
+
+更多的配置信息可以前往文档中查看，包括配置节点讲解，如何同步ES。
---
+++ maple-logback/src/main/java/com/maple/logback/MapleLogbackApplication.java
@@ -0,0 +1,16 @@
+package com.maple.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 笑小枫 <https://www.xiaoxiaofeng.com/>
+ */
+@SpringBootApplication
+public class MapleLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleLogbackApplication.class, args);
+    }
+
+}
